2282d9409716e9805407366487421be7c05d34d3
mikalikahi <sweeney.phil@gmail.com>
2023-05-29 16:45:41 +0800

    Added currency code field to savings account model

--- financial_accounts/models.py
+++ financial_accounts/models.py
@@ -9,11 +9,19 @@ class Bank(models.Model):
         return self.bank_name
 
 
+CURRENCY_TYPE = (
+    ('TWD', 'Taiwan New Dollar'),
+    ('USD', 'United States Dollar'),
+    ('EUR', 'Euro Member Countries'),
+    )
+
+
 class SavingsAccount(models.Model):
     bank = models.ForeignKey(Bank, on_delete=models.CASCADE)
     account_name = models.CharField(max_length=300)
     account_owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     account_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    currency_code = models.CharField(max_length=200, choices=CURRENCY_TYPE, default='TWD')
 
     def __str__(self):
         return "{}: {} Account ({})".format(
